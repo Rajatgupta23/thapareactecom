9325c47c154cae15c14dfe7760bda6e4bea850df
Rajatgupta23 <you@example.com>
2024-03-22 12:24:32 +0530

    Services and Trust

--- src/Component/Herosection.js
+++ src/Component/Herosection.js
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Button } from "../styles/Button";
+import Trusted from "./Trusted";
+import Services from "./Services";
 
 const HeroSection = ({ myData }) => {
   const { name } = myData;
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 import HeroSection from "./Component/Herosection";
+import Trusted from "./Component/Trusted";
+import Services from "./Component/Services";
 
 const Home = () => {
   const data = {
     name: "Ghar ki Dukan",
   };
 
-  return <HeroSection myData={data} />;
+  return <>
+            <HeroSection myData={data} />;
+            <Services/>
+            <Trusted/>
+        </> 
 };
 
 export default Home;
